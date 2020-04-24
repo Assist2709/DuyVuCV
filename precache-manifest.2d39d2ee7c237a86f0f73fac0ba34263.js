9b299481cf4d5a3d63923ef281327d4eb3f66e59
Assist2709 <khacduyland2019@gmail.com>
2020-04-24 11:31:51 +0700

    Updates

--- precache-manifest.2d39d2ee7c237a86f0f73fac0ba34263.js
+++ precache-manifest.2d39d2ee7c237a86f0f73fac0ba34263.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee0fec8298bcf844c3432d1380cde02b",
+    "revision": "8c431088ec76d41088eb30c151ddbe9a",
     "url": "/DuyVuCV/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/DuyVuCV/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "b06a4263735912581be1",
-    "url": "/DuyVuCV/static/css/main.82eb0dbb.chunk.css"
+    "revision": "e1c5b60444a2fbf7b910",
+    "url": "/DuyVuCV/static/css/main.e670e000.chunk.css"
   },
   {
     "revision": "f0f4a57b76b1c29f35e5",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/DuyVuCV/static/js/2.6b4bb886.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b06a4263735912581be1",
-    "url": "/DuyVuCV/static/js/main.347b3c28.chunk.js"
+    "revision": "e1c5b60444a2fbf7b910",
+    "url": "/DuyVuCV/static/js/main.d58a96e1.chunk.js"
   },
   {
     "revision": "f6012049952ff9b9f805",
